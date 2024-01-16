dbfbaea4f8e760c017ba2b3b6525bb7cd2ee5000
FabiolaLutrario <fabiolalutrario@gmail.com>
2024-01-16 10:47:39 -0300

    add comment on server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+// const cors = require("cors");
 require("dotenv").config();
 const db = require("./api/db");
 const app = express();
