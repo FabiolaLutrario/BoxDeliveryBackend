88e62ee77902eea7f04042f3f30963d655185c13
rosmari ledesma <rochilesde@gmail.com>
2024-01-22 11:22:21 -0300

    prueba 2

--- api/utils/function.js
+++ api/utils/function.js
@@ -4,3 +4,15 @@ function prueba(name) {
 
 console.log(prueba("rosmari ledesma"))
 
+function names(name) {
+  return `hello ${name}, como estas?`
+}
+
+console.log(names("rosmari ledesma"))
+
+function hello(name) {
+  return `hello ${name}, como estas?`
+}
+
+console.log(hello("rosmari ledesma"))
+
