ebacf533bd8ef338e0741a2993cdde2bf7b484d6
rosmari ledesma <rochilesde@gmail.com>
2024-02-06 10:05:57 -0300

    jwt implementation and private route

--- server.js
+++ server.js
@@ -1,29 +1,30 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-// const cors = require("cors");
+const cors = require("cors");
 require("dotenv").config();
 const db = require("./api/db");
 const app = express();
-
+const routes = require("./api/routes/index.js")
 app.use(express.json());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-/* app.use(
+ app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3001",
     credentials: true,
   })
-); */
-/* app.use((err, req, res, next) => {
+);
+ app.use("/api", routes);
+app.use((err, req, res,) => {
   res.status(500).send(err)
-}); */
+}); 
 // agrego ruta 
-app.get('/', (req, res) => {
-  res.status(200).send('OK');
-});
+// app.get('/', (req, res) => {
+//   res.status(200).send('OK');
+// });
 db.sync({ force: false }).then(() => {
   app.listen(process.env.PORT_LOCAL_APP, () =>
     console.log(`Server in port `, process.env.PORT_LOCAL_APP)
