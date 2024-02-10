7c195e2ec667fecd4db7b09b7f19a5788572ed1c
bautistagorchs <bautista.gorchs@gmail.com>
2024-02-10 18:38:20 -0300

    create User table in database

--- api/models/db.js
+++ api/models/db.js
@@ -1,15 +1,24 @@
 const Sequelize = require("sequelize");
+require("dotenv").config();
 
 const db = new Sequelize(
   process.env.POSTGRES_DB_NAME,
   process.env.POSTGRES_DB_USER,
   process.env.POSTGRES_DB_PASSWORD,
   {
-    host: "postgres-db",
+    host: "localhost",
     port: process.env.POSTGRES_DB_DOCKER_PORT,
     dialect: "postgres",
     logging: false,
   }
 );
+
+db.authenticate()
+  .then(() => {
+    console.log(`Successfully conected to database ${`appbox`}`);
+  })
+  .catch((err) => {
+    console.error("No se pudo conectar a la base de datos", err);
+  });
 
 module.exports = db;
--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const cors = require("cors");
 require("dotenv").config();
 const db = require("./api/models/db.js");
 const app = express();
-const routes = require("./api/routes/index.js");
+const routes = require("./api/routes/index.routes.js");
+
 app.use(express.json());
 app.use(logger("dev"));
 app.use(express.json());
@@ -18,13 +19,10 @@ app.use(
   })
 );
 app.use("/api", routes);
-app.use((err, req, res) => {
-  res.status(500).send(err);
+app.get("/health", (req, res) => {
+  res.status(200).send("The server is up and healthy 😀");
 });
-// agrego ruta
-// app.get('/', (req, res) => {
-//   res.status(200).send('OK');
-// });
+
 db.sync({ force: false }).then(() => {
   app.listen(process.env.PORT_LOCAL_APP, () =>
     console.log(`Server in port `, process.env.PORT_LOCAL_APP)
