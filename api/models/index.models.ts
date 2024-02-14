21eb755abd2bb3fbdfe40c8fefebeb154a8147f0
JuanPiPem <jplezcano75@gmail.com>
2024-02-14 05:52:26 -0800

    change values to value

--- api/models/index.models.ts
+++ api/models/index.models.ts
@@ -1,13 +1,13 @@
 import User from "./User.models";
 import Package from "./Package.models";
-import Values from "./Value.models";
+import Value from "./Value.models";
 import State from "./State.models";
 import City from "./City.models";
 
 Package.belongsTo(User, { foreignKey: "email_id", as: "user" });
-User.belongsTo(Values, { foreignKey: "confirmation_id", as: "values" });
-User.belongsTo(Values, { foreignKey: "user_Type_id", as: "value" });
+User.belongsTo(Value, { foreignKey: "confirmation_id", as: "value" });
+User.belongsTo(Value, { foreignKey: "user_Type_id", as: "value" });
 Package.belongsTo(State, { foreignKey: "state_id", as: "state" });
 Package.belongsTo(City, { foreignKey: "city_id", as: "city" });
 
-export default { User, Package, Values, State, City };
+export default { User, Package, Value, State, City };
