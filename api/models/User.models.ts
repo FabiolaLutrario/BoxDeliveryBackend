28c97dddfb37ff82c312590b457a1563ed9d666f
FabiolaLutrario <fabiolalutrario@gmail.com>
2024-02-17 16:10:18 -0300

    Changing model User

--- api/models/User.models.ts
+++ api/models/User.models.ts
@@ -1,46 +1,61 @@
 import S from "sequelize";
-import db from "../config/db";
+import db from "../config/db.config";
 
-class User extends S.Model {}
+class User extends S.Model {
+  email!: string;
+  name!: string;
+  last_name!: string;
+  password!: string;
+  salt?: string;
+  token?: string | null;
+  profile_photo?: string;
+  is_admin!: boolean;
+  is_confirmed!: boolean;
 
-User.init(
-  {
-    email: {
-      type: S.STRING,
-      primaryKey: true,
-      allowNull: false,
-    },
-    name: {
-      type: S.STRING,
-      allowNull: false,
-    },
-    last_name: {
-      type: S.STRING,
-      allowNull: false,
-    },
-    password: {
-      type: S.STRING,
-      allowNull: false,
-    },
-    salt: {
-      type: S.STRING,
-    },
-    token: {
-      type: S.STRING,
-    },
-    profile_photo: {
-      type: S.STRING,
-    },
-    isAdmin: {
-      type: S.BOOLEAN,
-      allowNull: false,
-    },
-    isConfirmed: {
-      type: S.BOOLEAN,
-      defaultValue: false,
-    },
-  },
-  { sequelize: db, modelName: "user", tableName: "user" }
-);
+  static initModel(): void {
+    this.init(
+      {
+        email: {
+          type: S.STRING,
+          primaryKey: true,
+          allowNull: false,
+        },
+        name: {
+          type: S.STRING,
+          allowNull: false,
+        },
+        last_name: {
+          type: S.STRING,
+          allowNull: false,
+        },
+        password: {
+          type: S.STRING,
+          allowNull: false,
+        },
+        salt: {
+          type: S.STRING,
+        },
+        token: {
+          type: S.STRING,
+        },
+        profile_photo: {
+          type: S.STRING,
+        },
+        is_admin: {
+          type: S.BOOLEAN,
+          allowNull: false,
+        },
+        is_confirmed: {
+          type: S.BOOLEAN,
+          allowNull: false,
+          defaultValue: false,
+        },
+      },
+      { sequelize: db, modelName: "user", tableName: "user" }
+    );
+  }
+}
+
+User.initModel();
 
 export default User;
