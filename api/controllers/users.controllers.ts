ebb0e174ba90b97bc9559d3cad00130c403b81cf
bautistagorchs <bautista.gorchs@gmail.com>
2024-02-19 12:35:40 -0300

    organize service and controllers routes for user

--- api/controllers/users.controllers.ts
+++ api/controllers/users.controllers.ts
@@ -1,8 +1,13 @@
 import jwt from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
 const secretKey = process.env.JWT_SECRET_KEY ?? "secret key";
+import * as userService from "../services/user.services";
 
-const verifyToken = (req: Request, res: Response, next: NextFunction) => {
+export const verifyToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const token = req.cookies.token;
   if (!token) {
     return res
@@ -22,4 +27,64 @@ const verifyToken = (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
-export default verifyToken;
+export const getAllUsers = (_req: Request, res: Response) => {
+  userService
+    .getAll()
+    .then((users: object) => res.status(200).send(users))
+    .catch((err: Error) => res.send(err));
+};
+
+export const registerUser = (req: Request, res: Response) => {
+  userService
+    .register(req.body)
+    .then(() => res.status(201).send("Created"))
+    .catch((err: Error) => res.status(500).send(err.message));
+};
+
+export const loginUser = (req: Request, res: Response) => {
+  userService
+    .login(req.body)
+    .then((response) =>
+      res.cookie("auth-token", response.token).send(response.message)
+    )
+    .catch((err) => res.status(401).send(err.message));
+};
+export const getDeliverymen = (_req: Request, res: Response) => {
+  userService
+    .getDeliverymen()
+    .then((deliverymen) => res.status(200).send(deliverymen))
+    .catch((err) => res.status(400).send(err));
+};
+
+export const getOneDeliverymen = (req: Request, res: Response) => {
+  userService
+    .getOneDeliverymen(req.params.id)
+    .then((deliverymen) => {
+      if (!deliverymen || deliverymen.isAdmin) return res.sendStatus(204);
+      return res.status(200).send(deliverymen);
+    })
+    .catch((err) => res.status(400).send(err));
+};
+
+export const deleteDeliverymen = (req: Request, res: Response) => {
+  userService
+    .deleteDeliverymen(req.body.email)
+    .then(() => res.status(200).send("Deliverymen deleted successfully"))
+    .catch(() =>
+      res.status(500).send("Failure when trying to delete delivery man")
+    );
+};
+
+export const deleteAdmin = (req: Request, res: Response) => {
+  const { email } = req.body;
+
+  userService
+    .deleteAdmin(email)
+    .then((response) => res.status(200).send(response))
+    .catch((err) => res.status(500).send(err.message));
+};
+
+export const logout = (_req: Request, res: Response) => {
+  res.clearCookie("auth-token");
+  res.sendStatus(200);
+};
--- api/routes/users.routes.ts
+++ api/routes/users.routes.ts
@@ -1,130 +1,22 @@
 import express from "express";
 const router = express.Router();
-import User from "../models/User.models";
-import { generateToken } from "../config/token.config";
+import * as userController from "../controllers/users.controllers";
 
-router.get("/all", (_req, res) => {
-  User.findAll()
-    .then((users: object) => res.status(200).send(users))
-    .catch((err: Error) => res.send(err));
-});
+router.get("/all", userController.getAllUsers);
 
-router.post("/register", (req, res) => {
-  const { email, name, last_name, password } = req.body;
+router.post("/register", userController.registerUser);
 
-  if (!email || !name || !last_name || !password)
-    return res.status(406).send("Please complete all fields");
+router.post("/login", userController.loginUser);
 
-  return User.findOne({ where: { email } })
-    .then((user) => {
-      if (user) {
-        return res
-          .status(400)
-          .send("There is already an account associated with this email");
-      }
-      return User.create(req.body)
-        .then(() => res.status(201).send("Created"))
-        .catch(() => {
-          //console.error("Error when trying to register user:", error);
-          return res.status(500).send("Internal Server Error");
-        });
-    })
-    .catch((err: Error) => res.send(err));
-});
+router.get("/deliverymen", userController.getDeliverymen);
 
-router.post("/login", (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password)
-    return res.status(406).send("Please complete all the fields");
-  return User.findOne({ where: { email } })
-    .then((user) => {
-      if (!user) return res.status(401).send("Incorrect email or password");
-      return user
-        .validatePassword(password)
-        .then((isOk) => {
-          if (!isOk) return res.status(401).send("Validation error, try again");
-          if (!user.isConfirmed)
-            return res
-              .status(401)
-              .send("Please confirm your account before trying to log in");
-          const payload = {
-            email,
-            name: user.name,
-            last_name: user.last_name,
-            isAdmin: user.isAdmin,
-            isConfirmed: user.isConfirmed,
-          };
-          const token = generateToken(payload);
-          res.cookie("auth-token", token).send("You are now logged in!");
-
-          return;
-          // res.status(200).send("All good");
-        })
-        .catch((err) => res.status(401).send(err));
-    })
-    .catch((err) => res.status(401).send(err));
-
-  // if (secretKey === undefined) {
-  //   return res.send("Internal server error, refresh and try again");
-  // } else {
-  //   const token = jwt.sign({ user }, secretKey, { expiresIn: "1h" });
-  //   return res.cookie("token", token).send({ token });
-  // }
-});
-
-router.get("/deliverymen", (_req, res) => {
-  User.findAll({ where: { isAdmin: false } })
-    .then((deliverymen) => res.status(200).send(deliverymen))
-    .catch((err) => res.status(400).send(err));
-});
-
-router.get("/deliverymen/:id", (req, res) => {
-  User.findOne({ where: { id: req.params.id } })
-    .then((deliverymen) => {
-      if (!deliverymen || deliverymen.isAdmin) return res.sendStatus(204);
-      return res.status(200).send(deliverymen);
-    })
-    .catch((err) => res.status(400).send(err));
-});
+router.get("/deliverymen/:id", userController.getOneDeliverymen);
 
 // faltan validaciones
-router.put("/delete/deliverymen", (req, res) => {
-  const { email } = req.body;
-  User.findOne({ where: { email } }).then((user) => {
-    if (user) {
-      return user
-        .destroy()
-        .then(() => res.status(200).send("Deliverymen deleted successfully"))
-        .catch(() =>
-          res.status(500).send("Failure when trying to delete delivery man")
-        );
-    }
-    return res
-      .status(400)
-      .send("We could not find the deliverymen asociated with that email");
-  });
-});
-
+router.delete("/delete/deliverymen", userController.deleteDeliverymen);
 // faltan validaciones
-router.put("/delete/admin", (req, res) => {
-  const { email } = req.body;
-  User.findOne({ where: { email, isAdmin: true } }).then((user) => {
-    if (user) {
-      return user
-        .destroy()
-        .then(() => res.status(200).send("Account deleted successfully"))
-        .catch(() =>
-          res.status(500).send("Failure when trying to delete account")
-        );
-    }
-    return res
-      .status(400)
-      .send("We could not find an account asociated with that email");
-  });
-});
+router.delete("/delete/admin", userController.deleteAdmin);
 
-router.post("/logout", (_req, res) => {
-  res.clearCookie("auth-token");
-  res.sendStatus(200);
-});
+router.post("/logout", userController.logout);
+
 export default router;
