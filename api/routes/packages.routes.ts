ebfdc4afcb2c7a34b4102549dff710447369e61a
FabiolaLutrario <fabiolalutrario@gmail.com>
2024-02-21 20:05:13 -0300

    Adding file docker-compose-dev.arm64.yml

--- api/routes/packages.routes.ts
+++ api/routes/packages.routes.ts
@@ -4,9 +4,11 @@ import { PackagesControllers } from "../controllers/packages.controllers";
 const router = express.Router();
 
 router.post("/add-package", PackagesControllers.addPackage);
+
 router.get("/", PackagesControllers.getAllPackages);
 
 router.get("/single/:id", PackagesControllers.getSinglePackage);
+
 router.get("/:user_id/:status", PackagesControllers.getPackagesByUserAndStatus);
 
 export default router;
