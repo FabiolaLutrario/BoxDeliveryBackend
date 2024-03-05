c8ee4725437e358fb4893c9fe768971678166ae2
FabiolaLutrario <fabiolalutrario@gmail.com>
2024-03-05 17:50:59 -0300

    Deleting comments into users.routes.ts

--- api/routes/users.routes.ts
+++ api/routes/users.routes.ts
@@ -1,34 +1,22 @@
 import { Router } from "express";
 const router = Router();
 import { UsersControllers } from "../controllers/users.controllers";
-
 import { validateAuth } from "../middlewares/auth";
 import { validateAuthAdmin } from "../middlewares/validateAuthAdmin";
 
 router.get("/", validateAuthAdmin, UsersControllers.getAllUsers);
 
-//Listo en swagger
 router.post("/register", UsersControllers.registerUser);
 
-//Listo en swagger
 router.put("/confirm-email/:token", UsersControllers.confirmEmail);
 
-//Listo en swagger
 router.post("/login", UsersControllers.loginUser);
 
-
 router.get("/deliverymen", validateAuth, UsersControllers.getDeliverymen);
-//Listo en swagger
 
-
-//Listo en swagger
 router.get("/single/:id", UsersControllers.getUser);
 
-router.get(
-  "/single-by-email/:email",
-
-  UsersControllers.getUserByEmail
-);
+router.get("/single-by-email/:email", UsersControllers.getUserByEmail);
 
 router.delete(
   "/delete/deliveryman",
@@ -38,22 +26,17 @@ router.delete(
 
 router.delete("/delete/admin", validateAuthAdmin, UsersControllers.deleteAdmin);
 
-//Listo en swagger
 router.post("/logout", UsersControllers.logout);
 
-//Listo en swagger
 router.put("/restore-password", UsersControllers.sendEmail);
 
-//Listo en swagger
 router.get(
   "/validate-token/:token",
   UsersControllers.validateTokenToRestorePassword
 );
 
-//Listo en swagger
 router.post("/overwrite-password/:token", UsersControllers.overwritePassword);
 
-//Listo en swagger
 router.get("/me", validateAuth, UsersControllers.me);
 
 export default router;
