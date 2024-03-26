3b75df26400e7c208cbe3dc16c2c23e499f93753
FabiolaLutrario <fabiolalutrario@gmail.com>
2024-03-26 19:50:27 -0300

    Using DB local

--- api/config/db.config.ts
+++ api/config/db.config.ts
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
-/* const db = new Sequelize(
+const db = new Sequelize(
   process.env.POSTGRES_DB_NAME ?? "appbox",
   process.env.POSTGRES_DB_USER ?? "postgres",
   process.env.POSTGRES_DB_PASSWORD ?? "appbox",
@@ -12,15 +12,15 @@ dotenv.config();
     dialect: "postgres",
     logging: false,
   }
-); */
+);
 
-const db = new Sequelize(
+/* const db = new Sequelize(
   "postgresql://uyw3zp4yythtmky2itkk:UzUqOZsvKFxjBKeRTgCn9x5JEYVC8v@bjuqfle74brsz7bhh74l-postgresql.services.clever-cloud.com:50013/bjuqfle74brsz7bhh74l",
   {
     dialect: "postgres",
     logging: false,
   }
-);
+); */
 
 db.authenticate()
   .then(() => {
